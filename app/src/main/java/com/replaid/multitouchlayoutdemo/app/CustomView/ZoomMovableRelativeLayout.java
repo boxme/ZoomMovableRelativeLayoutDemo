2e40d4c119fef529547c8d34c41690499488f49a
desmond <desmond@replaid.com>
2015-01-01 17:28:47 +0800

    Modify ZoomMoveableRelativeLayout to remove bugs

--- app/src/main/java/com/replaid/multitouchlayoutdemo/app/CustomView/ZoomMovableRelativeLayout.java
+++ app/src/main/java/com/replaid/multitouchlayoutdemo/app/CustomView/ZoomMovableRelativeLayout.java
@@ -12,31 +12,26 @@ import android.widget.RelativeLayout;
  * Created by desmond on 4/5/14.
  */
 public class ZoomMovableRelativeLayout extends RelativeLayout {
-    private static final String TAG = "ZoomMovableRelativeLayout";
+
     private static final int INVALID_POINTER_ID =  -1;
     private int mActivePointerId = INVALID_POINTER_ID;
 
-    //For Scaling
-    private float mScaleFactor = 1;
+    // For Scaling
+    private float mScaleFactor = 1.0f;
     private float mFocusX;
     private float mFocusY;
     private ScaleGestureDetector mScaleDetector;
     private Matrix mScaleMatrix;
     private Matrix mScaleMatrixInverse;
 
-    //For Moving
-    private float mPosX;
-    private float mPosY;
+    // For Moving
+    private float mTranslateX;
+    private float mTranslateY;
     private Matrix mTranslateMatrix;
     private Matrix mTranslateMatrixInverse;
 
     private float mLastTouchX;
     private float mLastTouchY;
-
-
-    private float[] mInvalidateWorkingArray = new float[6];
-    private float[] mDispatchTouchEventWorkingArray = new float[2];
-    private float[] mOnTouchEventWorkingArray = new float[2];
 
     public ZoomMovableRelativeLayout(Context context) {
         super(context);
@@ -63,45 +58,33 @@ public class ZoomMovableRelativeLayout extends RelativeLayout {
         mScaleDetector = new ScaleGestureDetector(context, new ScaleListener());
     }
 
+    public void scale(float scaleFactor, float focusX, float focusY) {
+        mScaleFactor = scaleFactor;
+        mFocusX = focusX;
+        mFocusY = focusY;
+        invalidate();
+    }
+
+    public void restore() {
+        mScaleFactor = 1.0f;
+        mTranslateX = 0f;
+        mTranslateY = 0f;
+        invalidate();
+    }
+
     @Override
     protected void dispatchDraw(Canvas canvas) {
-        //To draw the child view
+        // To draw the child view
         canvas.save(Canvas.MATRIX_SAVE_FLAG);
-        canvas.translate(mPosX, mPosY);
+        canvas.translate(mTranslateX, mTranslateY);
         canvas.scale(mScaleFactor, mScaleFactor, mFocusX, mFocusY);
         super.dispatchDraw(canvas);     //pass to child view
         canvas.restore();
     }
 
     @Override
-    public boolean dispatchTouchEvent(MotionEvent ev) {
-        //Pass the touchScreenEvent to the target view, or this if it's
-        //the target view
-        mDispatchTouchEventWorkingArray[0] = ev.getX();
-        mDispatchTouchEventWorkingArray[1] = ev.getY();
-
-        screenPointsToScaledPoints(mDispatchTouchEventWorkingArray);
-
-        ev.setLocation(mDispatchTouchEventWorkingArray[0],
-                       mDispatchTouchEventWorkingArray[1]);
-
-        return super.dispatchTouchEvent(ev);
-    }
-
-    private void screenPointsToScaledPoints(float[] array) {
-        mTranslateMatrixInverse.mapPoints(array);
-        mScaleMatrixInverse.mapPoints(array);
-    }
-
-    @Override
     public boolean onTouchEvent(MotionEvent ev) {
-        mOnTouchEventWorkingArray[0] = ev.getX();
-        mOnTouchEventWorkingArray[1] = ev.getY();
-
-        scalePointsToScreenPoints(mOnTouchEventWorkingArray);
-
-        ev.setLocation(mOnTouchEventWorkingArray[0], mOnTouchEventWorkingArray[1]);
-        //Listener to access all touchevent
+        // Listener to access all touchevent
         mScaleDetector.onTouchEvent(ev);
 
         final int action = ev.getAction();
@@ -119,22 +102,35 @@ public class ZoomMovableRelativeLayout extends RelativeLayout {
             }
 
             case MotionEvent.ACTION_MOVE: {
-                //Find the index of the active pointer and fetch its position
+                // Find the index of the active pointer and fetch its position
                 final int pointerIndex = ev.findPointerIndex(mActivePointerId);
                 final float x = ev.getX(pointerIndex);
                 final float y = ev.getY(pointerIndex);
 
-                final float dx = x - mLastTouchX;
-                final float dy = y - mLastTouchY;
+                if (!mScaleDetector.isInProgress()) {
+                    final float dx = x - mLastTouchX;
+                    final float dy = y - mLastTouchY;
 
-                mPosX += dx;
-                mPosY += dy;
-                mTranslateMatrix.preTranslate(dx, dy);
-                mTranslateMatrix.invert(mTranslateMatrixInverse);
+                    mTranslateX += dx;
+                    mTranslateY += dy;
 
-                //Update last position
-                mLastTouchX = x;
-                mLastTouchY = y;
+                    // Translate first; the sequence of translations/rotations/scales
+                    // are done in reverse order as you type them.
+                    // A method to apply the transformation in front of
+                    // all current transformations of the Matrix.
+                    // This method starts with pre followed by the name of the tranformationtype
+                    // A method to apply the transformation in the end of all
+                    // current transformations of the Matrix.
+                    // This method starts with post followed by the name of the tranformationtype
+                    // A method to apply the tranformation instead of all current
+                    // transformations of the Matrix.
+                    // This method starts with set followed by the name of the tranformationtype
+                    mTranslateMatrix.preTranslate(dx, dy);
+                    mTranslateMatrix.invert(mTranslateMatrixInverse);
+
+                    mLastTouchX = x;
+                    mLastTouchY = y;
+                }
 
                 invalidate();
                 break;
@@ -151,15 +147,15 @@ public class ZoomMovableRelativeLayout extends RelativeLayout {
             }
 
             case MotionEvent.ACTION_POINTER_UP: {
-                //Extract the index of the pointer that left the touch sensor
+                // Extract the index of the pointer that left the touch sensor
                 final int pointerIndex = (action & MotionEvent.ACTION_POINTER_INDEX_MASK)
                         >> MotionEvent.ACTION_POINTER_INDEX_SHIFT;
 
                 final int pointerId = ev.getPointerId(pointerIndex);
                 if (pointerId == mActivePointerId) {
-                    //This was our active pointer going up.
-                    //Choose a new active pointer and adjust accordingly
-                    //Choose the finger that is left on the screen
+                    // This was our active pointer going up.
+                    // Choose a new active pointer and adjust accordingly
+                    // Choose the finger that is left on the screen
                     final int newPointerIndex = pointerIndex == 0 ? 1 : 0;
                     mLastTouchX = ev.getX(newPointerIndex);
                     mLastTouchY = ev.getY(newPointerIndex);
@@ -171,29 +167,23 @@ public class ZoomMovableRelativeLayout extends RelativeLayout {
         return true;
     }
 
-    private void scalePointsToScreenPoints(float[] array) {
-        mScaleMatrix.mapPoints(array);
-        mTranslateMatrix.mapPoints(array);
-    }
-
     private class ScaleListener extends ScaleGestureDetector.SimpleOnScaleGestureListener {
         @Override
         public boolean onScale(ScaleGestureDetector detector) {
             mScaleFactor *= detector.getScaleFactor();
 
             if (detector.isInProgress()) {
-                //Content to zoom about the focal point of the gesture
+                // Content to zoom about the focal point of the gesture
                 mFocusX = detector.getFocusX();
                 mFocusY = detector.getFocusY();
             }
-            //Prevent from scaling to too big or small a size
+            // Prevent from scaling to too big or small a size
             mScaleFactor = Math.max(0.1f, Math.min(mScaleFactor, 5.0f));
 
             mScaleMatrix.setScale(mScaleFactor, mScaleFactor, mFocusX, mFocusY);
             mScaleMatrix.invert(mScaleMatrixInverse);
 
-            invalidate();
-            requestLayout();   //Invalidate the layout of this view and its child views
+            scale(mScaleFactor, mFocusX, mFocusY);
 
             return true;
         }
